c548f0184016ae92b455a43d5e3ff3796e0a69a3
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2017-05-27 16:19:49 +0300

    added tmux support and removed user input, user must use arguments when calls the script

--- serialComunication/serialCom.sh
+++ serialComunication/serialCom.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
+# if there is any error exit
+set -e
+
 port="ttyUSB0";
 baud="9600";
+
+# run mannually in a shell
+# find which port arduino uses
+# ls -l /sys/bus/usb-serial/devices/
+
+# change port's permissions
+# sudo chmod 666 /dev/port
+
 
 
 function viewMsg {
@@ -9,7 +20,9 @@ function viewMsg {
 	sleep 2
 	stty -F /dev/$port raw
 	sleep 2
-	cat < /dev/$port
+	while true; do
+		cat < /dev/$port
+	done
 }
 
 # sendMsg function will only work if viewMsg is running too in some other tty 
@@ -19,20 +32,34 @@ function sendMsg {
 		echo "Enter message >"
 		read msg
 		echo $msg > /dev/$port
+		echo "wait"
 		sleep 3 # wait for the buffer to get cleaned
 	done
 	
 }
 
-echo  "Enter 1 to view messages| 2 to send messages"
-read option
+# use tmux to see in one pane the messages that the board sends
+# and in another pane the commands prompt 
+function openTmux {
+	session="SerialCom"
+	tmux -2 new-session -d -s $session
+	tmux rename-window "main"
+	tmux send-keys "./serialCom.sh 1" C-m
+	tmux split-window -v # change to -h if your prefer horizontal
+	tmux send-keys "./serialCom.sh 2" C-m
+	tmux attach -t $session
+}
 
-if [ $option == 1 ]
+
+if [ "$1" == "1" ]
 then
 	viewMsg
-elif [ $option == 2 ]
+elif [ "$1" == "2" ]
 then
 	sendMsg
+elif [ "$1" == "3" ]
+then
+	openTmux
 else
 	echo "unknown input"
 fi
