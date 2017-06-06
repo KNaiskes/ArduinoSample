6e8dd5985b4c0e023c0033fb72c85ef16a4eaf92
Kiriakos <kiriakosnaiskes@gmail.com>
2017-06-06 11:52:57 +0000

    Added laser to the project and modified its code

--- pirSensor/pirSensor.ino
+++ pirSensor/pirSensor.ino
@@ -4,6 +4,8 @@
    the blue led is high while sensor detects no movements but if it does then
    blue led is turned low and the red led is turned high while the pir sensor
    detects any movement
+
+   Added a laser to fire up when movement is detected
    */
 
 const int redLed = 8;
@@ -11,12 +13,18 @@ const int blueLed= 6;
 const int pirPin = 7;
 int status = 0;
 
+//laser
+const int laserPin = 2;
+
 void setup()
 {
 	pinMode(redLed,OUTPUT);
 	pinMode(blueLed,OUTPUT);
 	pinMode(pirPin,INPUT);
+	pinMode(laserPin,OUTPUT);
 	digitalWrite(redLed,LOW);
+	digitalWrite(laserPin,LOW);
+
 }
 
 void loop()
@@ -25,6 +33,7 @@ void loop()
 
 	if(status == LOW){
 		digitalWrite(redLed,LOW);
+		digitalWrite(laserPin,LOW);
 		digitalWrite(blueLed,HIGH);
 	}
 
@@ -32,8 +41,9 @@ void loop()
 		status = digitalRead(pirPin);
 		digitalWrite(blueLed,LOW);
 		digitalWrite(redLed,HIGH);
+		digitalWrite(laserPin,HIGH);
 		if(status == LOW){
-			delay(1000); // do not turn low the red led immediately 
+			delay(2000); // do not turn low the red led immediately 
 			break;
 		}
 	}
