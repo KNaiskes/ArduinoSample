994f412fd93b5d78cef94b696d704cca08785998
Kiriakos <kiriakosnaiskes@gmail.com>
2017-08-11 16:43:22 +0300

    added two sensors functions

--- buzzAlarm/buzzAlarm.ino
+++ buzzAlarm/buzzAlarm.ino
@@ -5,9 +5,54 @@
 
 const int buzzerPin = 7;
 
-void setup() {}
+// pir sensor
+const int pirPin = 8;
+int status;
 
+// distance sensor
+const int echoPin = 5;
+const int triggerPin = 6;
+
+
+
+void pirSensor(int pir ) {
+	pir = digitalRead(pirPin);
+	while(pir == HIGH) {
+		pir = digitalRead(pirPin);
+		tone(buzzerPin,400,700);
+		delay(2000);
+		tone(buzzerPin,450,650);
+		delay(2000);
+		tone(buzzerPin,300,500);
+		delay(2000);
+	}
+}
+
+void distanceSensor(int trig, int echo) {
+	long duration;
+	int distance;
+
+	duration = pulseIn(echo,HIGH);
+	digitalWrite(trig,HIGH);
+	digitalWrite(trig,LOW);
+
+	distance = duration * 0.034 / 2;
+	if(distance < 5) {
+		tone(buzzerPin,500,500);
+	}
+
+}
+
+void setup() {
+	// pir sensor
+	//pinMode(pirPin,INPUT);
+	// distance sensor
+	pinMode(triggerPin,OUTPUT);
+	pinMode(echoPin,INPUT);
+
+}
+	
 void loop() {
-	tone(buzzerPin,2000,700);
-	delay(1000);
+	//pirSensor(status);
+	distanceSensor(triggerPin,echoPin);
 }
