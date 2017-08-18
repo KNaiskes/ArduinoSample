b4a3b62049f5b23c1caf93d5870c252fe1168341
Kiriakos <kiriakosnaiskes@gmail.com>
2017-08-18 22:06:33 +0300

    added a simple test sketch for relay

---
+++ relay/relay.ino
@@ -0,0 +1,16 @@
+#include<Arduino.h>
+
+const int c1 = 7;
+
+void setup() {
+	pinMode(r1,OUTPUT);
+	digitalWrite(c1,LOW);
+}
+
+void loop() {
+
+	digitalWrite(c1,LOW);
+	delay(3000);
+	digitalWrite(c1,HIGH);
+	delay(3000);
+}
